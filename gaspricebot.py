07be8f7bf68703e092ad66a98fba4589f57b4fba
Dylan Nguyen <93560178+thedigitalchief@users.noreply.github.com>
2022-04-12 07:42:32 -0700

    fixed typos, cleaned up formatting

--- gaspricebot.py
+++ gaspricebot.py
@@ -8,6 +8,7 @@ logger = logging.getLogger(__file__)
 
 PRICE_URL = 'https://calculator.aa.co.za/calculators-toolscol-1/fuel-pricing'
 
+#so Chrome or gas webpage doesnt know this is a bot
 options = webdriver.ChromeOptions()
 options.add_argument("--no-sandbox")
 options.add_argument("--headless")
@@ -15,6 +16,7 @@ options.add_argument("--window-size=1920,1080")
 options.add_argument("--disable-extensions")
 
 
+#function that loads, clicks, retrieves data
 def get_prices(driver: webdriver):
     """
     Get Price Data from AA Website.
@@ -22,6 +24,7 @@ def get_prices(driver: webdriver):
     :param driver: Chromedriver
     :return:
     """
+    
     fuel_price_data = []
     driver.get(PRICE_URL)
     location_elements = driver.find_elements_by_xpath("//div[@id='edit-location']/div/input")
@@ -30,6 +33,7 @@ def get_prices(driver: webdriver):
     fuel_types = [fuel_.get_attribute("value") for fuel_ in fuel_elements]
     year_elements = driver.find_elements_by_xpath("//select[@id='edit-year']/option")
     years = [year.get_attribute("value") for year in year_elements]
+    
     for location in locations:
         driver.find_element_by_xpath(f"//input[@value='{location}']/following-sibling::label").click()
         for fuel in fuel_types:
