460c371db636a08ee1713ddd6ccaa48a9d5cd459
thedigitalchief <dylannguyn@gmail.com>
2022-04-13 00:46:15 -0700

    defined more variables

--- gaspricebot.py
+++ gaspricebot.py
@@ -9,7 +9,7 @@ logger = logging.getLogger(__file__)
 
 PRICE_URL = 'https://calculator.aa.co.za/calculators-toolscol-1/fuel-pricing'
 
-#so Chrome or gas webpage doesnt know this is a bot
+#setting parameters so Chrome and webpage detect botting
 options = webdriver.ChromeOptions()
 options.add_argument("--no-sandbox")
 options.add_argument("--headless")
@@ -28,10 +28,13 @@ def get_prices(driver: webdriver):
     
     fuel_price_data = []
     driver.get(PRICE_URL)
+
     location_elements = driver.find_elements_by_xpath("//div[@id='edit-location']/div/input")
     locations = [location.get_attribute("value") for location in location_elements]
+
     fuel_elements = driver.find_elements_by_xpath("//select[@id='edit-fuel-type']/option")
     fuel_types = [fuel_.get_attribute("value") for fuel_ in fuel_elements]
+    
     year_elements = driver.find_elements_by_xpath("//select[@id='edit-year']/option")
     years = [year.get_attribute("value") for year in year_elements]
     
@@ -43,14 +46,15 @@ def get_prices(driver: webdriver):
                 driver.find_element_by_xpath(f"//select[@id='edit-year']/option[@value='{year}']").click()
                 driver.find_element_by_xpath("//input[@value='Get Fuel Price']").click()
                 print(f"Getting Data for {location}:{fuel}:{year}.")
-                time.sleep(3)
+                time.sleep(3) #should always have min 3 sec sleep
                 rows = driver.find_elements_by_xpath("//tbody/tr")
                 for row in rows:
                     _row = row.get_attribute("innerText").split("\n")
                     fuel_price_data.append(_row + [location, fuel, year])
 
                     
-#function that analyzes and displays gas prices                    
+# function that analyzes and displays gas prices    
+# Data wrangling is the process of cleaning and unifying messy and complex data sets for easy access and analysis.                
 def wrangle_data(data: list):
     """
     Wrangle/Transform DAta from Price Data.
